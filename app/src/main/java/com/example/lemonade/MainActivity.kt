e0013b6adc7818fc722bc52b7df0459fd3fd849b
owner <fatcat7@aol.jp>
2023-12-22 04:38:55 +0900

    Fixed Ripple stick out

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -136,9 +137,9 @@ fun LemonApp() {
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
+                    .clip(shape = RoundedCornerShape(12.dp))
                     .background(
                         color = Color(0xFFC3ECD2),
-                        shape = RoundedCornerShape(12.dp)
                     )
                     .clickable(
                         onClick = onClick
